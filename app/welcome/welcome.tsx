2510347f9e1f275c3d505148dc7693e94fd0dba5
Krunal Beladiya <103812794+Krunal5001@users.noreply.github.com>
2026-04-19 16:33:15 +0530

    Spelling Change

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function Welcome() {
   useEffect(() => {
-    window.location.href = "/Menu.pdf";
+    window.location.href = "/MENU.pdf";
   }, []);
 
   return null;
